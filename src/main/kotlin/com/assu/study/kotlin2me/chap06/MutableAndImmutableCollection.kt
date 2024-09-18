0f02973403e30c34f6180d1373da1616ec84779c
assu10 <juhyun1010@naver.com>
2024-09-18 20:12:53 +0900

    3.1. 읽기 전용 컬렉션과 변경 가능한 컬렉션: `Collection`, `MutableCollection`

---
+++ src/main/kotlin/com/assu/study/kotlin2me/chap06/MutableAndImmutableCollection.kt
@@ -0,0 +1,21 @@
+package com.assu.study.kotlin2me.chap06
+
+// src 컬렉션은 변경하지 않지만, target 컬레교션은 변경한다는 사실을 알 수 있음
+fun <T> copyElements(
+    src: Collection<T>,
+    target: MutableCollection<T>,
+) {
+    for (item in src) {
+        target.add(item)
+    }
+}
+
+fun main() {
+    val src: Collection<Int> = listOf(1, 2, 3)
+    val target: MutableCollection<Int> = mutableListOf(4, 5)
+
+    copyElements(src, target)
+
+    println(src) // [1, 2, 3]
+    println(target) // [4, 5, 1, 2, 3]
+}
