df0981a00ae3a43d74ddbbb8b217f1ff4e00304a
assu10 <juhyun1010@naver.com>
2024-10-09 14:49:07 +0900

    3.1. 인라이닝이 동작하는 방식

---
+++ src/main/kotlin/com/assu/study/kotlin2me/chap08/Inlining.kt
@@ -0,0 +1,31 @@
+package com.assu.study.kotlin2me.chap08
+
+import java.util.concurrent.locks.Lock
+
+inline fun <T> customSynchronized(
+    lock: Lock,
+    action: () -> T,
+): T {
+    lock.lock()
+
+    try {
+        return action()
+    } finally {
+        lock.unlock()
+    }
+}
+
+fun foo(lock: Lock) {
+    println("Before sync")
+    customSynchronized(lock) {
+        println("Action")
+    }
+    println("After sync")
+}
+
+fun main() {
+    val lock = Lock()
+    customSynchronized(lock) {
+        // ..
+    }
+}
