84702d299ae31080365338abcd5d99fd5ef6c1dc
assu10 <juhyun1010@naver.com>
2024-10-27 16:08:20 +0900

    애너테이션을 활용한 JSON 직렬화 제어

--- src/test/kotlin/com/assu/study/kotlin2me/chap10/jkid/examples/PersonTest.kt
+++ src/test/kotlin/com/assu/study/kotlin2me/chap10/jkid/examples/PersonTest.kt
@@ -1,6 +1,8 @@
 package com.assu.study.kotlin2me.chap10.jkid.examples
 
 import org.junit.jupiter.api.Test
+import ru.yole.jkid.JsonExclude
+import ru.yole.jkid.JsonName
 import ru.yole.jkid.deserialization.deserialize
 import ru.yole.jkid.serialization.serialize
 import kotlin.test.assertEquals
@@ -8,6 +10,13 @@ import kotlin.test.assertEquals
 data class Person(
     val name: String,
     val age: Int,
+)
+
+data class Person2(
+    @JsonName("alias")
+    val firstName: String,
+    @JsonExclude
+    val age: Int? = null,
 )
 
 class PersonTest {
@@ -23,9 +32,24 @@ class PersonTest {
         // 클래스를 명시해야 함 (아래에서는 Person 클래스를 타입 인자로 넘김)
         // Person(name=Alice, age=29)
         println(deserialize<Person>(json))
-        // println(deserialize(json, Person::class))
 
         assertEquals(json, serialize(person))
         assertEquals(person, deserialize(json))
+    }
+
+    @Test
+    fun test2() {
+        val person2 = Person2("Lee", 20)
+        val json = """{"alias": "Lee"}"""
+        val json2 = """{"alias": "Lee", "age": 20}"""
+
+        // {"alias": "Lee"}
+        println(serialize(person2))
+
+        // Person2(firstName=Lee, age=null)
+        println(deserialize<Person2>(json))
+
+        assertEquals(json, serialize(person2))
+        assertEquals(person2, deserialize(json2))
     }
 }
